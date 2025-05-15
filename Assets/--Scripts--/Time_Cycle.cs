d5ced073df553560c1c460e86de6c3724e93e47b
WilliamScoones <williamahlertmusic@gmail.com>
2025-05-15 20:43:02 +0100

    Finished designing level + puzzles
    Still to do;
    
    Collision on environmental objects
    
    True day-night cycle upon completion of all puzzles (commented code doesn't quite do it)
    
    Implementation of audio systems proper.

--- Assets/--Scripts--/Time_Cycle.cs
+++ Assets/--Scripts--/Time_Cycle.cs
@@ -3,10 +3,12 @@ using DG.Tweening;
 
 public class Time_Cycle : MonoBehaviour
 {
-    private float cycleTime;
+    private float cycleTime = 0;
     public Transform dirLight;
+    public int puzzlesSolved = 0;
     private float counter;
 
+    public int x;
     void Update()
     {
         /*
@@ -19,11 +21,24 @@ public class Time_Cycle : MonoBehaviour
             cycleTime = 0;
         }
         */
-    }
+        /*
+        cycleTime += Time.deltaTime * 15;
+        dirLight.DORotate(new Vector3(cycleTime, -180, 0), 10f);
 
+        if(cycleTime >= 360)
+        {
+            cycleTime = 0;
+        }
+    }
+*/
+    }
     public void CycleTime()
     {
-        counter += 40;
-        dirLight.DORotate(new Vector3(counter, -180, 0), 10f);
+        if (puzzlesSolved < 8)
+        {
+            counter += 60;
+            dirLight.DORotate(new Vector3(counter, -180, 0), 10f);
+            puzzlesSolved++;
+        }
     }
 }
